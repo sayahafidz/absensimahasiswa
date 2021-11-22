4e76dbd646dba1bf8038719e9a7811db6ae378d9
Hafidz Alfatih <hafidzalfatih04@gmail.com>
2021-11-22 23:22:57 +0700

    admin edit absensi, export excel, fix profil mahasiswa

--- admin_delete_mahasiswa.php
+++ admin_delete_mahasiswa.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'koneksi.php';
-$nim = $_GET['nim'];
-$result = mysqli_query($koneksi, "DELETE FROM mahasiswa WHERE nim='$nim'");
+$id = $_GET['id'];
+$result = mysqli_query($koneksi, "DELETE FROM mahasiswa WHERE id='$id'");
 if ($result) {
     header('location:admin_index.php?page=admin_mahasiswa&pesan=berhasil_delete');
 }else{
